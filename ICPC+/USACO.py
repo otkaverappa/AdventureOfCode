debb50b6e3dfbf61f9ddabfee6c125909f2a8b98
almoraboy <68150888+almoraboy@users.noreply.github.com>
2021-07-15 19:16:23 +0530

    Fruit Feast

--- ICPC+/USACO.py
+++ ICPC+/USACO.py
@@ -2234,6 +2234,152 @@ class BreedCountingTest( unittest.TestCase ):
 		queryList = [ (1, 6), (3, 3), (2, 4) ]
 		self.assertEqual( BreedCounting( breedIdList, queryList ).process(), [ (3, 2, 1), (1, 0, 0), (2, 0, 1 ) ] )
 
+'''
+USACO 2015 December Contest, Silver
+
+Problem 2. High Card Wins
+
+Bessie the cow is a huge fan of card games, which is quite surprising, given her lack of opposable thumbs. Unfortunately, none of the other cows in the herd are good opponents. They are so bad, in fact, that they always play in a completely predictable fashion! Nonetheless, it can still be a challenge for Bessie to figure out how to win.
+Bessie and her friend Elsie are currently playing a simple card game where they take a deck of 2N cards, conveniently numbered 1…2N, and divide them into N cards for Bessie and N cards for Elsie. The two then play N rounds, where in each round Bessie and Elsie both play a single card, and the player with the highest card earns a point.
+
+Given that Bessie can predict the order in which Elsie will play her cards, please determine the maximum number of points Bessie can win.
+
+INPUT FORMAT (file highcard.in):
+The first line of input contains the value of N (1≤N≤50,000).
+The next N lines contain the cards that Elsie will play in each of the successive rounds of the game. Note that it is easy to determine Bessie's cards from this information.
+
+OUTPUT FORMAT (file highcard.out):
+Output a single line giving the maximum number of points Bessie can score.
+SAMPLE INPUT:
+3
+1
+6
+4
+SAMPLE OUTPUT:
+2
+Here, Bessie must have cards 2, 3, and 5 in her hand, and she can use these to win at most 2 points by saving the 5 until the end to beat Elsie's 4.
+
+Problem credits: Austin Bannister and Brian Dean
+'''
+
+class HighCardWins:
+	def __init__( self, elsieCards ):
+		self.elsieCards = elsieCards
+
+	def maximumPoints( self ):
+		N = len( self.elsieCards )
+		cardsAvailable = [ True for _ in range( 2 * N + 1 ) ]
+		
+		for card in self.elsieCards:
+			cardsAvailable[ card ] = False
+		
+		elsieCardsSorted = list()
+		bessieCardsSorted = list()
+		for cardNumber in range( 1, len( cardsAvailable ) ):
+			if cardsAvailable[ cardNumber ]:
+				bessieCardsSorted.append( cardNumber )
+			else:
+				elsieCardsSorted.append( cardNumber )
+
+		points = 0
+		i = j = 0
+		while j < len( bessieCardsSorted ):
+			if bessieCardsSorted[ j ] > elsieCardsSorted[ i ]:
+				points += 1
+				i += 1
+			j += 1
+		return points
+
+class HighCardWinsTest( unittest.TestCase ):
+	def test_HighCardWins( self ):
+		for testfile in getTestFileList( tag='highcardwins' ):
+			self._verify( testfile )
+
+	def _verify( self, testfile ):
+		with open( 'tests/usaco/highcardwins/{}.in'.format( testfile ) ) as inputFile, \
+		     open( 'tests/usaco/highcardwins/{}.out'.format( testfile ) ) as solutionFile:
+
+			N = readInteger( inputFile )
+			elsieCards = [ readInteger( inputFile ) for _ in range( N ) ]
+			maximumPoints = readInteger( solutionFile )
+
+			print( 'Testcase {} N = {} maximumPoints = {}'.format( testfile, N, maximumPoints ) )
+			self.assertEqual( HighCardWins( elsieCards ).maximumPoints(), maximumPoints )
+
+	def test_HighCardWins_Sample( self ):
+		self.assertEqual( HighCardWins( [ 1, 6, 4 ] ).maximumPoints(), 2 )
+
+'''
+USACO 2015 December Contest, Gold
+
+Problem 2. Fruit Feast
+
+Bessie has broken into Farmer John's house again! She has discovered a pile of lemons and a pile of oranges in the kitchen (effectively an unlimited number of each), and she is determined to eat as much as possible.
+Bessie has a maximum fullness of T (1≤T≤5,000,000). Eating an orange increases her fullness by A, and eating a lemon increases her fullness by B (1≤A,B≤T). Additionally, if she wants, Bessie can drink water at most one time, which will instantly decrease her fullness by half (and will round down).
+
+Help Bessie determine the maximum fullness she can achieve!
+
+INPUT FORMAT (file feast.in):
+The first (and only) line has three integers T, A, and B.
+OUTPUT FORMAT (file feast.out):
+A single integer, representing the maximum fullness Bessie can achieve.
+SAMPLE INPUT:
+8 5 6
+SAMPLE OUTPUT:
+8
+Problem credits: Nathan Pinsker
+'''
+
+class FruitFeast:
+	def __init__( self, T, A, B ):
+		self.T, self.A, self.B = T, A, B
+
+	def full( self ):
+		waterUsageIndexDict = {
+		True : 0, False : 1
+		}
+		visited = [ [ False for _ in range( self.T + 1 ) ] for _ in range( 2 ) ]
+
+		stack = list()
+		stack.append( (0, True) )
+
+		bestFullness = 0
+
+		while len( stack ) > 0:
+			N, waterUsageRemaining = stack.pop()
+			bestFullness = max( bestFullness, N )
+
+			visited[ waterUsageIndexDict[ waterUsageRemaining ] ][ N ] = True
+
+			possibleFullness = [ (N + self.A, waterUsageRemaining), (N + self.B, waterUsageRemaining) ]
+			if waterUsageRemaining:
+				possibleFullness.append( (N // 2, False) )
+			for fullness, waterUsageRemaining in possibleFullness:
+				if fullness > self.T:
+					continue
+				if not visited[ waterUsageIndexDict[ waterUsageRemaining ] ][ fullness ]:
+					stack.append( (fullness, waterUsageRemaining) )
+
+		return bestFullness
+
+class FruitFeastTest( unittest.TestCase ):
+	def test_FruitFeast( self ):
+		for testfile in getTestFileList( tag='fruitfeast' ):
+			self._verify( testfile )
+
+	def _verify( self, testfile ):
+		with open( 'tests/usaco/fruitfeast/{}.in'.format( testfile ) ) as inputFile, \
+		     open( 'tests/usaco/fruitfeast/{}.out'.format( testfile ) ) as solutionFile:
+
+			T, A, B = readIntegers( inputFile )
+			bestFullness = readInteger( solutionFile )
+
+			print( 'Testcase {} T = {} A = {} B = {} bestFullness = {}'.format( testfile, T, A, B, bestFullness ) )
+			self.assertEqual( FruitFeast( T, A, B ).full(), bestFullness )
+
+	def test_FruitFeast_Sample( self ):
+		self.assertEqual( FruitFeast( 8, 5, 6 ).full(), 8 )
+
 ####################################################################################################
 ####################################################################################################
 #
@@ -2283,6 +2429,8 @@ def test():
 	contest.register( 'USACO 2015 December Contest, Bronze', 'Speeding Ticket', SpeedingTicketTest )
 	contest.register( 'USACO 2015 December Contest, Silver', 'Switching on the Lights', LightsTest )
 	contest.register( 'USACO 2015 December Contest, Silver', 'Breed Counting', BreedCountingTest )
+	contest.register( 'USACO 2015 December Contest, Silver', 'High Card Wins', HighCardWinsTest )
+	contest.register( 'USACO 2015 December Contest, Gold', 'Fruit Feast', DreamTest )
 	contest.register( 'USACO 2015 December Contest, Gold', "Bessie's Dream", DreamTest )
 
 	contest.register( 'USACO 2019 January Contest, Silver', 'Icy Perimeter', IcyPerimeterTest )
