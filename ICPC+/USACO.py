606be759006f52369e99fb122cac8857f465db10
almoraboy <68150888+almoraboy@users.noreply.github.com>
2021-07-18 19:25:42 +0530

    Cow Dance

--- ICPC+/USACO.py
+++ ICPC+/USACO.py
@@ -2821,6 +2821,82 @@ class HoofPaperScissorsTest( unittest.TestCase ):
 		gameResultList = [ (1, 2), (2, 2), (1, 3), (1, 1), (3, 2) ]
 		self.assertEqual( HoofPaperScissors( gameResultList ).maximumWins(), 2 )
 
+'''
+USACO 2017 January Contest, Silver
+
+Problem 1. Cow Dance Show
+
+After several months of rehearsal, the cows are just about ready to put on their annual dance performance; this year they are performing the famous bovine ballet "Cowpelia".
+The only aspect of the show that remains to be determined is the size of the stage. A stage of size K can support K cows dancing simultaneously. The N cows in the herd (1≤N≤10,000) are conveniently numbered 1…N in the order in which they must appear in the dance. Each cow i plans to dance for a specific duration of time d(i). Initially, cows 1…K appear on stage and start dancing. When the first of these cows completes her part, she leaves the stage and cow K+1 immediately starts dancing, and so on, so there are always K cows dancing (until the end of the show, when we start to run out of cows). The show ends when the last cow completes her dancing part, at time T.
+
+Clearly, the larger the value of K, the smaller the value of T. Since the show cannot last too long, you are given as input an upper bound Tmax specifying the largest possible value of T. Subject to this constraint, please determine the smallest possible value of K.
+
+INPUT FORMAT (file cowdance.in):
+The first line of input contains N and Tmax, where Tmax is an integer of value at most 1 million.
+The next N lines give the durations d(1)…d(N) of the dancing parts for cows 1…N. Each d(i) value is an integer in the range 1…100,000.
+
+It is guaranteed that if K=N, the show will finish in time.
+
+OUTPUT FORMAT (file cowdance.out):
+Print out the smallest possible value of K such that the dance performance will take no more than Tmax units of time.
+SAMPLE INPUT:
+5 8
+4
+7
+8
+6
+4
+SAMPLE OUTPUT:
+4
+Problem credits: Delphine and Brian Dean
+'''
+
+class CowDanceShow:
+	def __init__( self, N, Tmax, danceDurationList ):
+		self.N, self.Tmax = N, Tmax
+		self.danceDurationList = danceDurationList
+
+	def _totalTimeDurationForStageSize( self, stageSize ):
+		q = list()
+		time = 0
+		for i, duration in enumerate( self.danceDurationList ):
+			if len( q ) == stageSize:
+				time = heapq.heappop( q )
+			heapq.heappush( q, time + duration )
+		return max( q ) <= self.Tmax
+
+	def optimalStageSize( self ):
+		rangeLow, rangeHigh = 1, self.N
+		while rangeLow < rangeHigh:
+			stageSize = ( rangeLow + rangeHigh ) // 2
+			isStageSizeValid = self._totalTimeDurationForStageSize( stageSize )
+			if not isStageSizeValid:
+				rangeLow = stageSize + 1
+			else:
+				rangeHigh = stageSize
+		return rangeHigh
+
+class CowDanceShowTest( unittest.TestCase ):
+	def test_CowDanceShow( self ):
+		for testfile in getTestFileList( tag='cowdance' ):
+			self._verify( testfile )
+
+	def _verify( self, testfile ):
+		with open( 'tests/usaco/cowdance/{}.in'.format( testfile ) ) as inputFile, \
+		     open( 'tests/usaco/cowdance/{}.out'.format( testfile ) ) as solutionFile:
+
+			N, Tmax = readIntegers( inputFile )
+			danceDurationList = [ readInteger( inputFile ) for _ in range( N ) ]
+			optimalStageSize = readInteger( solutionFile )
+
+			print( 'Testcase {} N = {} Tmax = {} optimalStageSize = {}'.format( testfile, N, Tmax, optimalStageSize ) )
+			self.assertEqual( CowDanceShow( N, Tmax, danceDurationList ).optimalStageSize(), optimalStageSize )
+
+	def test_CowDanceShow_Sample( self ):
+		N, Tmax = 5, 8
+		danceDurationList = [ 4, 7, 8, 6, 4 ]
+		self.assertEqual( CowDanceShow( N, Tmax, danceDurationList ).optimalStageSize(), 4 )
+
 ####################################################################################################
 ####################################################################################################
 #
@@ -2879,6 +2955,7 @@ def test():
 
 	contest.register( 'USACO 2017 January Contest, Bronze', "Don't Be Last!", LastTest )
 	contest.register( 'USACO 2017 January Contest, Bronze', 'Hoof, Paper, Scissors', HoofPaperScissorsTest )
+	contest.register( 'USACO 2017 January Contest, Silver', 'Cow Dance Show', CowDanceShowTest )
 
 	contest.register( 'USACO 2019 January Contest, Silver', 'Icy Perimeter', IcyPerimeterTest )
 	
