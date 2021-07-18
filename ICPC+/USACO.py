d2d9befbbd0d5eed2bfd8150ef91d6e9c2d58423
almoraboy <68150888+almoraboy@users.noreply.github.com>
2021-07-18 16:46:33 +0530

    Last

--- ICPC+/USACO.py
+++ ICPC+/USACO.py
@@ -2656,6 +2656,171 @@ class CowChecklistTest( unittest.TestCase ):
 		positionList2 = [ (0, 3), (1, 3) ]
 		self.assertEqual( CowChecklist( positionList1, positionList2 ).minimumEnergy(), 20 )
 
+'''
+USACO 2017 January Contest, Bronze
+
+Problem 1. Don't Be Last!
+
+Farmer John owns 7 dairy cows: Bessie, Elsie, Daisy, Gertie, Annabelle, Maggie, and Henrietta. He milks them every day and keeps detailed records on the amount of milk provided by each cow during each milking session. Not surprisingly, Farmer John highly prizes cows that provide large amounts of milk.
+Cows, being lazy creatures, don't necessarily want to be responsible for producing too much milk. If it were up to them, they would each be perfectly content to be the lowest-producing cow in the entire herd. However, they keep hearing Farmer John mentioning the phrase "farm to table" with his human friends, and while they don't quite understand what this means, they have a suspicion that it actually may not be the best idea to be the cow producing the least amount of milk. Instead, they figure it's safer to be in the position of producing the second-smallest amount of milk in the herd. Please help the cows figure out which of them currently occupies this desirable position.
+
+INPUT FORMAT (file notlast.in):
+The input file for this task starts with a line containing the integer N (1≤N≤100), giving the number of entries in Farmer John's milking log.
+Each of the N following lines contains the name of a cow (one of the seven above) followed by a positive integer (at most 100), indicating the amount of milk produced by the cow during one of its milking sessions.
+
+Any cow that does not appear in the log at all is assumed to have produced no milk.
+
+OUTPUT FORMAT (file notlast.out):
+On a single line of output, please print the name of the cow that produces the second-smallest amount of milk. More precisely, if M is the minimum total amount of milk produced by any cow, please output the name of the cow whose total production is minimal among all cows that produce more than M units of milk. If several cows tie for this designation, or if no cow has this designation (i.e., if all cows have production equal to M), please output the word "Tie". Don't forget to add a newline character at the end of your line of output. Note that M=0 if one of the seven cows is completely absent from the milking log, since this cow would have produced no milk.
+SAMPLE INPUT:
+10
+Bessie 1
+Maggie 13
+Elsie 3
+Elsie 4
+Henrietta 4
+Gertie 12
+Daisy 7
+Annabelle 10
+Bessie 6
+Henrietta 5
+SAMPLE OUTPUT:
+Henrietta
+In this example, Bessie, Elsie, and Daisy all tie for the minimum by each producing 7 units of milk. The next-largest production, 9 units, is due to Henrietta.
+
+Problem credits: Brian Dean
+'''
+
+class Last:
+	def __init__( self, milkingLog ):
+		self.milkingLog = milkingLog
+		self.tie = 'Tie'
+		self.totalCows = 7
+
+	def analyze( self ):
+		milkDict = dict()
+		for cow, amountOfMilk in self.milkingLog:
+			if cow not in milkDict:
+				milkDict[ cow ] = 0
+			milkDict[ cow ] += amountOfMilk
+
+		milkList = [ (amountOfMilk, cow) for cow, amountOfMilk in milkDict.items() ]
+		milkList.sort( reverse=True )
+
+		if len( milkList ) < self.totalCows:
+			milkList.append( (0, None) )
+
+		minimumMilk, _ = milkList.pop()
+		while len( milkList ) > 0:
+			amountOfMilk, _ = milkList[ -1 ]
+			if amountOfMilk == minimumMilk:
+				milkList.pop()
+			else:
+				break
+
+		if len( milkList ) == 0:
+			return self.tie
+		optimalMilk, cow = milkList.pop()
+		if len( milkList ) == 0:
+			return cow
+		else:
+			amountOfMilk, _ = milkList.pop()
+			return cow if amountOfMilk != optimalMilk else self.tie
+
+class LastTest( unittest.TestCase ):
+	def test_Last( self ):
+		for testfile in getTestFileList( tag='last' ):
+			self._verify( testfile )
+
+	def _verify( self, testfile ):
+		with open( 'tests/usaco/last/{}.in'.format( testfile ) ) as inputFile, \
+		     open( 'tests/usaco/last/{}.out'.format( testfile ) ) as solutionFile:
+
+			N = readInteger( inputFile )
+			milkingLog = list()
+			for _ in range( N ):
+				cow, amountOfMilk = readString( inputFile ).split()
+				milkingLog.append( (cow, int( amountOfMilk )) )
+			bestCow = readString( solutionFile )
+
+			print( 'Testcase {} N = {} bestCow = {}'.format( testfile, N, bestCow ) )
+			self.assertEqual( Last( milkingLog ).analyze(), bestCow )
+
+	def test_Last_Sample( self ):
+		milkingLog = [
+		('Bessie', 1), ('Maggie', 13), ('Elsie', 3), ('Elsie', 4), ('Henrietta', 4), ('Gertie', 12),
+		('Daisy', 7), ('Annabelle', 10), ('Bessie', 6,), ('Henrietta', 5)
+		]
+		self.assertEqual( Last( milkingLog ).analyze(), 'Henrietta' )
+
+'''
+USACO 2017 January Contest, Bronze
+
+Problem 2. Hoof, Paper, Scissors
+
+You have probably heard of the game "Rock, Paper, Scissors". The cows like to play a similar game they call "Hoof, Paper, Scissors".
+The rules of "Hoof, Paper, Scissors" are simple. Two cows play against each-other. They both count to three and then each simultaneously makes a gesture that represents either a hoof, a piece of paper, or a pair of scissors. Hoof beats scissors (since a hoof can smash a pair of scissors), scissors beats paper (since scissors can cut paper), and paper beats hoof (since the hoof can get a papercut). For example, if the first cow makes a "hoof" gesture and the second a "paper" gesture, then the second cow wins. Of course, it is also possible to tie, if both cows make the same gesture.
+
+Farmer John watches in fascination as two of his cows play a series of N games of "Hoof, Paper, Scissors" (1≤N≤100). Unfortunately, while he can see that the cows are making three distinct types of gestures, he can't tell which one represents "hoof", which one represents "paper" and which one represents "scissors" (to Farmer John's untrained eye, they all seem to be variations on "hoof"...)
+
+Not knowing the meaning of the three gestures, Farmer John assigns them numbers 1, 2, and 3. Perhaps gesture 1 stands for "hoof", or maybe it stands for "paper"; the meaning is not clear to him. Given the gestures made by both cows over all N games, please help Farmer John determine the maximum possible number of games the first cow could have possibly won, given an appropriate mapping between numbers and their respective gestures.
+
+INPUT FORMAT (file hps.in):
+The first line of the input file contains N.
+Each of the remaining N lines contain two integers (each 1, 2, or 3), describing a game from Farmer John's perspective.
+
+OUTPUT FORMAT (file hps.out):
+Print the maximum number of games the first of the two cows could possibly have won.
+SAMPLE INPUT:
+5
+1 2
+2 2
+1 3
+1 1
+3 2
+SAMPLE OUTPUT:
+2
+One solution (of several) for this sample case is to have 1 represent "scissors", 2 represent "hoof", and 3 represent "paper". This assignment gives 2 victories to the first cow ("1 3" and "3 2"). No other assignment leads to more victories.
+
+Problem credits: Brian Dean
+'''
+
+class HoofPaperScissors:
+	def __init__( self, gameResultList ):
+		self.gameResultList = gameResultList
+
+	def maximumWins( self ):
+		winningMoves = set( [ (1, 2), (2, 3), (3, 1) ] )
+		wins = losses = 0
+		for (x, y) in self.gameResultList:
+			if x == y:
+				continue
+			if (x, y) in winningMoves:
+				wins += 1
+			else:
+				losses += 1
+		return max( wins, losses )
+
+class HoofPaperScissorsTest( unittest.TestCase ):
+	def test_HoofPaperScissors( self ):
+		for testfile in getTestFileList( tag='hoof' ):
+			self._verify( testfile )
+
+	def _verify( self, testfile ):
+		with open( 'tests/usaco/hoof/{}.in'.format( testfile ) ) as inputFile, \
+		     open( 'tests/usaco/hoof/{}.out'.format( testfile ) ) as solutionFile:
+
+			N = readInteger( inputFile )
+			gameResultList = [ tuple( readIntegers( inputFile ) ) for _ in range( N ) ]
+			maximumWins = readInteger( solutionFile )
+
+			print( 'Testcase {} N = {} maximumWins = {}'.format( testfile, N, maximumWins ) )
+			self.assertEqual( HoofPaperScissors( gameResultList ).maximumWins(), maximumWins )
+
+	def test_HoofPaperScissors_Sample( self ):
+		gameResultList = [ (1, 2), (2, 2), (1, 3), (1, 1), (3, 2) ]
+		self.assertEqual( HoofPaperScissors( gameResultList ).maximumWins(), 2 )
+
 ####################################################################################################
 ####################################################################################################
 #
@@ -2711,6 +2876,9 @@ def test():
 	contest.register( 'USACO 2015 December Contest, Gold', "Bessie's Dream", DreamTest )
 
 	contest.register( 'USACO 2016 December Contest, Gold', 'Cow Checklist', CowChecklistTest )
+
+	contest.register( 'USACO 2017 January Contest, Bronze', "Don't Be Last!", LastTest )
+	contest.register( 'USACO 2017 January Contest, Bronze', 'Hoof, Paper, Scissors', HoofPaperScissorsTest )
 
 	contest.register( 'USACO 2019 January Contest, Silver', 'Icy Perimeter', IcyPerimeterTest )
 	
